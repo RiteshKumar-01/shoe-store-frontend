3dc3ef8c2aca234ab11246703c3656a0e25e5705
Ritesh Kumar <rk7212103@gmail.com>
2023-09-26 01:29:05 +0530

    adding final commit in frontend

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,24 +1,18 @@
-import HeroBanner from "@/components/HeroBanner";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
+import React from "react";
 
-export default function Home() {
+const category = () => {
   return (
-    <main>
-      <HeroBanner />
+    <div className="w-full md:py-20 relative">
       <Wrapper>
-        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+        <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
           <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
-            Cushioning for Your Miles
-          </div>
-          <div className="text-md md:text-xl">
-            A lightweight Nike ZoomX midsole is combined with increased stack
-            heights to help provide cushioning during extended stretches of
-            running.
+            Running Shoes
           </div>
         </div>
 
-        {/* Product Grid Start */}
+        {/* products grid start */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
           <ProductCard />
           <ProductCard />
@@ -30,8 +24,10 @@ export default function Home() {
           <ProductCard />
           <ProductCard />
         </div>
-        {/* Product Grid End */}
+        {/* products grid end */}
       </Wrapper>
-    </main>
+    </div>
   );
-}
+};
+
+export default category;
